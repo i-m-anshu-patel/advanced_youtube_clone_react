26aca6208bc038a79b7f64cd2c47f29e7d086f84
anshu <manu22425@gmail.com>
2024-01-26 23:25:21 +0530

    Header done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-
+import { useDispatch } from 'react-redux'
+import { toggleMenu } from '../utils/redux/appSlice';
 const Header = () => {
-    const menuToggle = useSelector((store) => store.app.isMenuOpen);
+  const dispatch = useDispatch();
   return (
-    <div>
-     {menuToggle ? (<h1>"yes"</h1>) : (<h1>"no"</h1>)}
+    <div className='grid grid-cols-12 pt-2'>
+      <div className='flex col-span-4'>
+      <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1024px-Hamburger_icon.svg.png' alt='hamburger-btn' className='h-10 w-10 mx-2' onClick={() => {dispatch(toggleMenu())}}/>
+     <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/2560px-Logo_of_YouTube_%282015-2017%29.svg.png' alt='logo' className='h-10 w-24'/>
+      </div>
+    <div className='col-span-8 '>
+      <input type='text' placeholder='Search' className='border w-1/2 p-2 rounded-l-full'/>
+      <button className='bg-gray-300 py-2 px-4 rounded-r-full'><i className="fa fa-search "></i></button>
+    </div>
     </div>
   )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Sidebar from './Sidebar'
+import MainContainer from './MainContainer'
+import { useSelector } from 'react-redux';
 
 const Home = () => {
+  const menuToggle = useSelector((store) => store.app.isMenuOpen);
   return (
-    <div>
-      Home
+    <div className="grid grid-cols-12">
+      {menuToggle && (<div className='col-span-2'><Sidebar/></div>)}
+      <div className={menuToggle ? "col-span-10" : "col-span-12"}><MainContainer /></div>
     </div>
   )
 }
