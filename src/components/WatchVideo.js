d736646c8c77b05a6e267588f946a8b69c063c9b
anshu <manu22425@gmail.com>
2024-01-29 14:33:55 +0530

    custom hook for time conversion

--- src/components/WatchVideo.js
+++ src/components/WatchVideo.js
@@ -3,15 +3,11 @@ import { useLocation } from 'react-router-dom'
 import VideoContainer from './VideoContainer';
 import VideoList from './VideoList';
 import { useDispatch } from 'react-redux';
-import { addCategoryId } from '../utils/redux/categorySlice';
+import { addVideo } from '../utils/redux/videoSlice';
 
 const WatchVideo = () => {
     const {state} = useLocation();
     const { video } = state;
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(addCategoryId(video?.snippet?.categoryId))
-    }, [])
   return (
     <div className='grid grid-cols-12 gap-3'>
       <VideoContainer video={video} />
---
+++ src/utils/hooks/useConvertDuration.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react'
+
+const useConvertDuration = (duration) => {
+    const [datetime, setDateTime] = useState();
+    const convertDuration = (duration) => {
+        const regex = /PT(\d+)M(\d+)S/;
+        const match = duration.match(regex);
+        if (match) {
+          const minutes = parseInt(match[1]);
+          const seconds = parseInt(match[2]);
+          setDateTime(minutes + ":" + seconds);
+        }
+      }
+      useEffect(() => {
+            convertDuration(duration);
+      }, [duration])
+
+      return datetime;
+}
+
+export default useConvertDuration
